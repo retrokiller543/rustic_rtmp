346cdc9d50f23f089796df21680a3c280c9ea821
Retrokiller543 <emil.schutt@gmail.com>
2023-08-11 17:02:12 +0200

    chore: fixed more things....

--- increment_version.sh
+++ increment_version.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 tag=$1
-echo "Tag: $tag"
 
-major=$(echo $tag | cut -d. -f1 | cut -c2-)
+major=$(echo $tag | cut -d. -f1)
 minor=$(echo $tag | cut -d. -f2)
 patch=$(echo $tag | cut -d. -f3)
 
@@ -14,18 +13,15 @@ minor_count=$(echo "$messages" | grep -cE "^(fix|minor):")
 patch_count=$(echo "$messages" | grep -cE "^(chore|refactor|patch):")
 
 if [[ $major_count -ge $minor_count ]] && [[ $major_count -ge $patch_count ]]; then
-  echo "Incrementing major version $major_count $minor_count $patch_count"
   # Increment the major number
   major=$((major + 1))
   minor=0
   patch=0
 elif [[ $minor_count -ge $major_count ]] && [[ $minor_count -ge $patch_count ]]; then
-  echo "Incrementing minor version"
   # Increment the minor number
   minor=$((minor + 1))
   patch=0
 else
-  echo "Incrementing patch version"
   # Increment the patch number
   patch=$((patch + 1))
 fi
@@ -33,6 +29,11 @@ fi
 version="$major.$minor.$patch"
 
 # Update the version in Cargo.toml
+echo "Updating version to $version"
+echo "messages = $messages"
 sed -i "s/^version = \".*\"/version = \"$version\"/" Cargo.toml
 
-echo "NEW_VERSION=$version" >> $GITHUB_ENV
+# Only try to set the GitHub Actions environment variable if running within GitHub Actions
+if [ ! -z "$GITHUB_ENV" ]; then
+  echo "NEW_VERSION=$version" >> $GITHUB_ENV
+fi
